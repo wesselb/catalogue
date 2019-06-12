49309d5acb8f4634a7714b556febc2c97f38a3f4
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-06-12 16:01:55 +0100

    Move to new package structure

--- setup.py
+++ setup.py
@@ -4,20 +4,11 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
-    readme = f.read()
+requirements = ['numpy',
+                'titlecase',
+                'bs4',
+                'bibtexparser',
+                'html5lib']
 
-with open('LICENCE') as f:
-    licence = f.read()
-
-setup(
-    name='catalogue',
-    version='0.1.0',
-    description='Resource management with Alfred',
-    long_description=readme,
-    author='Wessel Bruinsma',
-    author_email='wessel.p.bruinsma@gmail.com',
-    url='https://github.com/wesselb/catalogue',
-    license=licence,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+setup(packages=find_packages(exclude=['docs']),
+      install_requires=requirements)
