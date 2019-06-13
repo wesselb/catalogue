e6b4a434be87e39a7a5877cdf44c162820258775
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-06-13 18:24:54 +0100

    Tighten requirements and increase version

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-requirements = ['numpy',
+requirements = ['numpy>=1.16',
                 'titlecase',
                 'bs4',
                 'bibtexparser',
                 'html5lib']
 
 setup(packages=find_packages(exclude=['docs']),
-      install_requires=requirements)
+      install_requires=requirements,
+      include_package_data=True)
